c66620d78772d8ef703c188deffa134a37812eaa
Gayath <gayath.chandira94@gmail.com>
2019-02-25 18:46:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Contacts from './components/contacts/contacts';
+import Header from './components/layout/header';
+import AddContact from './components/contacts/addContact';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from './context';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider>
+        <Router>
+          <div>
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
